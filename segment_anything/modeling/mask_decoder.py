98439a88601f4290b63152ab48b1efc88e4c6cd9
Noha-Magdy <N.Magdy@nu.edu.eg>
2023-06-27 01:56:52 +0300

    Update mask_decoder.py

--- segment_anything/modeling/mask_decoder.py
+++ segment_anything/modeling/mask_decoder.py
@@ -124,7 +124,10 @@ class MaskDecoder(nn.Module):
 
         # Expand per-image data in batch direction to be per-mask
         src = torch.repeat_interleave(image_embeddings, tokens.shape[0], dim=0)
-        src = src + dense_prompt_embeddings
+        if image_embeddings.shape[0] != tokens.shape[0]:
+            src = torch.repeat_interleave(image_embeddings, tokens.shape[0], dim=0)
+        else:
+            src = image_embeddings
         pos_src = torch.repeat_interleave(image_pe, tokens.shape[0], dim=0)
         b, c, h, w = src.shape
 
